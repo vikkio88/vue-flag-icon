598565f09bc288f4c7a26a1c96a49dc18b6bae8c
Rafał Olszewski <olszak94@gmail.com>
2022-05-18 20:40:25 +0200

    Update css classes

--- components/icon/Flag.vue
+++ components/icon/Flag.vue
@@ -1,22 +1,22 @@
-<style>
-
-</style>
 <template>
-    <span v-if="iso" class="flag-icon" :class="flagIconClass" :title="title || iso">
-    </span>
+  <span v-if="iso" class="fi" :class="flagIconClass" :title="title || iso">
+  </span>
 </template>
+
 <script>
 export default {
-    name: 'flag',
-    props: {
-        iso: { type: String, default: null },
-        title: { type: String, default: null },
-        squared: { type: Boolean, default: true },
+  name: "flag",
+  props: {
+    iso: { type: String, default: null },
+    title: { type: String, default: null },
+    squared: { type: Boolean, default: true },
+  },
+  computed: {
+    flagIconClass() {
+      return (!!this.squared ? "fis " : "") + "fi-" + this.iso.toLowerCase();
     },
-    computed: {
-        flagIconClass: function() {
-            return ((!!this.squared) ? 'flag-icon-squared ' : '') + 'flag-icon-' + this.iso.toLowerCase();
-        }
-    }
-}
+  },
+};
 </script>
+
+<style></style>
